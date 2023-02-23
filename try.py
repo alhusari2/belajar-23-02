b81ab588467890eddfaa352495829046fa1b323f
Dian <alhusari2@gmail.com>
2023-02-23 14:29:49 +0700

    pembaharuan file try.py

--- try.py
+++ try.py
@@ -25,3 +25,69 @@ kk = KalkulatorKali()
  
 b = kk.tambah_angka(5, 6)  # fitur tambah_angka yang dipanggil milik KalkulatorKali
 print(b)
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+def fa (net, teta):
+    if (net<-teta) :
+       return -1;
+    elif (net>teta):
+       return 1;
+    else:
+       return 0;
+
+X=np.array ([[1,1], [1,-1], [-1,1],[-1,-1]])
+bdata=len(X);
+teta=0.1
+print ("Banyak data: ",bdata)
+bfeat=len (X [0,:])
+print ("Banyak Feature: ",bfeat)
+T=np.array([1,-1,-1,-1])
+print (X)
+print (T)
+alfa=float (input ("Learning rate:"))
+
+w=np.zeros (2)
+b=np.random.rand ()
+for i in range (2):
+   w[i]=np.random.rand ()
+print ("Bobot Awal:",w)
+print ("Bias Awal:",b)
+berhenti=False
+ItMax=100
+it=0
+while (berhenti==False and it<ItMax):
+    berhenti = True
+    faktif=np.zeros (bdata)
+    for i in range (bdata):
+        net=b
+        for j in range (bfeat):
+          net=net+w[j] *X[i,j]
+        faktif[i]=fa (net, teta)
+        if faktif[i]!=T[i]:
+          berhenti = False
+        for j in range (bfeat):
+          w[j] =w[j]+alfa*T[i]*X[i,j]
+        b=b+alfa*T[i]
+        it=it+1
+print ("Jumlah iterasi=",it)
+print ("Bobot Akhir:",w)
+print ("Bias Akhir:",b)
+plt.scatter (X[:,0],T,s=100, c='b', marker='o')
+plt.scatter (X[ :,0],faktif, s=100, c='y', marker='*')
+plt.xlabel ("Feature 1 (x1)")
+plt.ylabel("Keluaran Y/T")
+plt.title ("Perbandingan Keluaran Jaringan Perceptron dan Target");
+plt.show ()
+
+#Testing
+print ("Tahap Testing")
+XTest=np.zeros (2)
+XTest [0]=float (input ("Input X1="))
+XTest [1]=float (input ("Input X2="))
+net=b
+for j in range (bfeat) :
+    net=net+w[j]*XTest[j]
+Out=fa (net,teta)
+print ("Keluaran :",Out)
